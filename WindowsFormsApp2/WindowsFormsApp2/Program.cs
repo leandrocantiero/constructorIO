6187caafd05ca76a21295ef1748adf1cea2f26f6
navas <gabrielnavas@email.com>
2020-05-13 09:04:24 -0300

    corrigir tela de inicialização

--- WindowsFormsApp2/WindowsFormsApp2/Program.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Program.cs
@@ -17,9 +17,8 @@ namespace WindowsFormsApp2.View
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            //Application.Run(new Login());
+            Application.Run(new Login());
 
-            Application.Run(new IniciarObra());
             /*
              INSERTS -----------------------------------------------------------
 
